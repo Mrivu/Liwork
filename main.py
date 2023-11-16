db69d8b4b7c93be306d08c0846c04719db6166a9
Van Uden Iivari <iivari.vanuden@edu.valkeakoski.fi>
2023-11-16 20:58:32 +0200

    fix box and add thickness

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
 import os
-from pyfiglet import figlet_format
+import math
+#from pyfiglet import figlet_format
 
-def Bar(vertical=False,rowcount=0,material="-",endmaterial=None):
+def Bar(material=None,thickness=1):
     width, height = os.get_terminal_size()
     if material == None: material="-"
-    if vertical:
-        if endmaterial==None: endmaterial=material
-        print((material + " "*(width - 2) + endmaterial + "\t")*rowcount)
-    else:
-        print(material * (width - 2))
+    print((material * math.floor(width/len(material)))*thickness)
+    
+def VBar(rowcount,material="-",endmaterial=None,thickness=1):
+    width, height = os.get_terminal_size()
+    if endmaterial==None: endmaterial=material
+    print((material*thickness + " "*(width - 2*thickness) + endmaterial*thickness + "\t")*rowcount)
 
-def Asciitxt(text,font,center):
+def Box(boxtext=[],material="-",boxSizeByLargestString=True):
+    width, height = os.get_terminal_size()
+    largestString = 0
+    for text in boxtext:
+        if len(text) > largestString: largestString = len(text)
+    slotstring = ""
+    looper = 0
+    for text in boxtext:
+        divideAdd = 0
+        if (largestString/2)%1 != 0 and len(boxtext[looper]) != largestString and (len(boxtext[looper])/2)%1 == 0:
+            divideAdd = 1
+        if boxSizeByLargestString:
+            slotstring += (("-"*((int((width-len(boxtext)*(largestString+len(material)*2))/(len(boxtext)+1)))) + "["+int((largestString-len(boxtext[looper]))/2)*" "+boxtext[looper]+int((largestString-len(boxtext[looper]))/2)*" "+divideAdd*" "+"]"))
+        else:
+            slotstring += (("-"*((int((width-len(boxtext)*(largestString+len(material)*2))/(len(boxtext)+1)))) + "["+boxtext[looper]+"]"))
+        looper += 1
+    #PrintBox
+    Bar(material)
+    print(slotstring+material*(width-len(material)-len(slotstring))+material)
+    Bar(material)
+
+#def Asciitxt(text,font,center):
     #0: left, 1: center, 2: right
-    width, height = os.get_terminal_size()
-    if center:
-        return figlet_format(text,font=font).center(width)
-    else:
-        return figlet_format(text,font=font)
+    #width, height = os.get_terminal_size()
+    #if center:
+        #return figlet_format(text,font=font).center(width)
+    #else:
+        #return figlet_format(text,font=font)
 
-def Box(slots,material="-"):
-    width, height = os.get_terminal_size()
-    slotstring = ((" "*int((width-2-slots*10)/slots) + "[        ]")*slots)
-    Bar(False,0,material)
-    print(material+slotstring+material)
-    Bar(False,0,material)
-
-Box(1)
-Bar(True,1,"[","]")
+Box(["ability 1: slash", "ability 259: 'Press w to activate'"],"-",True)
+Bar("w",1)
+VBar(2,"[","]",1)
